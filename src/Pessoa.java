313762a00b1167bb0c632248b0b34dcfb02eb172
Francisco Gabriel <galvesa9@gmail.com>
2024-02-11 12:36:20 -0300

    criando metodos da classe Pessoa.java

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -5,6 +5,23 @@ public class Pessoa {
     private Date dataNascimento;
 
     public Pessoa(String nome, Date dataNascimento){
+        this.dataNascimento = dataNascimento;
+        this.nome = nome;
+    }
 
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 }
